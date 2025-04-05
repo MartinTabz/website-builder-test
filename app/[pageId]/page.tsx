a7c2ab19dd130bbd4c2fc7426bd6154f7e640d14
Martin Šíl <mail@martinsil.cz>
2025-04-05 22:22:22 +0200

    Adding full Supabase SSR

--- app/[pageId]/page.tsx
+++ app/[pageId]/page.tsx
@@ -1,7 +1,7 @@
 import EditorProvider from "@/providers/editor/editor-provider";
 import { notFound } from "next/navigation";
 import PageEditorNavigation from "./_components/page-editor-navigation";
-import { getSupabase } from "@/lib/supabase";
+import { createClient } from "@/utils/supabase/server";
 
 type Props = {
 	params: Promise<{ pageId: string }>;
@@ -10,18 +10,13 @@ type Props = {
 export default async function FunnelPage({ params }: Props) {
 	const { pageId } = await params;
 
-	const supabase = getSupabase();
+	const supabase = await createClient();
 
-	const { data: pageDetails, error: pageErr } = await supabase
+	const { data: pageDetails } = await supabase
 		.from("pages")
 		.select("*")
 		.eq("id", pageId)
 		.single();
-
-	if (pageErr && pageErr.details != "The result contains 0 rows") {
-		console.error(pageErr);
-		throw new Error("Něco se pokazilo při načítání stránky");
-	}
 
 	if (!pageDetails) {
 		notFound();
