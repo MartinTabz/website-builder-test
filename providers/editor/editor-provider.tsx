e5efd814503f75c85ecedc317751f67840856454
Martin Šíl <mail@martinsil.cz>
2025-04-05 20:43:31 +0200

    Adds element addition functionality to editorReducer
    Implements the ability to add new elements to the editor state,
    including updating the history for undo/redo functionality.
    
    The implementation uses a reducer function to handle the "ADD_ELEMENT" action.
    The reducer updates the editor's element array by recursively searching for the target container and appending the new element.
    
    It also updates the history stack, allowing users to undo and redo element additions.

--- providers/editor/editor-provider.tsx
+++ providers/editor/editor-provider.tsx
@@ -62,3 +62,72 @@ const initialState: EditorState = {
 	editor: initialEditorState,
 	history: initialHistoryState,
 };
+
+
+
+const addAnElement = (
+	editorArray: EditorElement[],
+	action: EditorAction
+): EditorElement[] => {
+	if (action.type !== "ADD_ELEMENT") {
+		throw Error(
+			"You sent the wrong action type to the Add Element editor State"
+		);
+	}
+	return editorArray.map((item) => {
+		if (item.id === action.payload.containerId && Array.isArray(item.content)) {
+			return {
+				...item,
+				content: [...item.content, action.payload.elementDetails],
+			};
+		} else if (item.content && Array.isArray(item.content)) {
+			return {
+				...item,
+				content: addAnElement(item.content, action),
+			};
+		}
+		return item;
+	});
+};
+
+const editorReducer = (
+	state: EditorState = initialState,
+	action: EditorAction
+): EditorState => {
+	switch (action.type) {
+		case "ADD_ELEMENT":
+			const updatedEditorState = {
+				...state.editor,
+				elements: addAnElement(state.editor.elements, action),
+			};
+			const updatedHistory = [
+				...state.history.history.slice(0, state.history.currentIndex + 1),
+				{ ...updatedEditorState },
+			];
+
+			const newEditorState = {
+				...state,
+				editor: updatedEditorState,
+				history: {
+					...state.history,
+					history: updatedHistory,
+					currentIndex: updatedHistory.length - 1,
+				},
+			};
+			return newEditorState;
+
+		case "UPDATE_ELEMENT":
+		case "DELETE_ELEMENT":
+		case "CHANGE_CLICKED_ELEMENT":
+		case "CHANGE_DEVICE":
+		case "TOGGLE_PREVIEW_MODE":
+		case "TOGGLE_LIVE_MODE":
+		case "REDO":
+		case "UNDO":
+		case "LOAD_LOCALSTORAGE":
+		case "LOAD_DATA":
+		case "SET_PAGE_ID":
+		default:
+			return state;
+	}
+};
