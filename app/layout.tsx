01362022b950ffa066f2434ed98377ea21bb6126
Martin Šíl <mail@martinsil.cz>
2025-04-05 22:25:33 +0200

    Enables theme switching with persistence
    Adds a theme provider to allow users to switch between themes and persist their choice.
    Also adds a notification component for displaying messages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
 	title: "Website Builder Test",
@@ -12,8 +14,18 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="cs">
-			<body className={`antialiased`}>{children}</body>
+		<html lang="cs" suppressHydrationWarning>
+			<body className={`antialiased`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+					<Toaster />
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
