1f429b9f5724f5684b5e141e52e517d386ffaa48
Martin Šíl <mail@martinsil.cz>
2025-04-06 21:59:38 +0200

    Adds base for page editor

---
+++ app/[pageId]/_components/page-editor/index.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEditor } from "@/providers/editor/editor-provider";
+import { createClient } from "@/utils/supabase/client";
+import clsx from "clsx";
+import { EyeOff } from "lucide-react";
+import { useEffect } from "react";
+
+type Props = {
+	pageId: string;
+	liveMode?: boolean;
+};
+
+export default function PageEditor({ pageId, liveMode }: Props) {
+	const { dispatch, state } = useEditor();
+	const supabase = createClient();
+
+	useEffect(() => {
+		if (liveMode) {
+			dispatch({
+				type: "TOGGLE_LIVE_MODE",
+				payload: { value: true },
+			});
+		}
+	}, [liveMode]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const { data } = await supabase
+				.from("pages")
+				.select("content")
+				.eq("id", pageId)
+				.single();
+
+			if (!data) {
+				return;
+			}
+
+			dispatch({
+				type: "LOAD_DATA",
+				payload: {
+					elements:
+						typeof data.content === "string"
+							? JSON.parse(data.content)
+							: data.content || [],
+					withLive: !!liveMode,
+				},
+			});
+		}
+
+		fetchData();
+	}, [pageId]);
+
+	const handleClick = () => {
+		dispatch({
+			type: "CHANGE_CLICKED_ELEMENT",
+			payload: {},
+		});
+	};
+
+	const handleUnpreview = () => {
+		dispatch({
+			type: "TOGGLE_PREVIEW_MODE",
+		});
+		dispatch({
+			type: "TOGGLE_LIVE_MODE",
+		});
+	};
+
+	return (
+		<div
+			className={clsx(
+				"use-automation-zoom-in h-full overflow-scroll mr-[385px] bg-background transition-all rounded-md",
+				{
+					"p-0 !mr-0":
+						state.editor.previewMode === true || state.editor.liveMode === true,
+					"!w-[850px]": state.editor.device === "Tablet",
+					"!w-[420px]": state.editor.device === "Mobile",
+					"w-full": state.editor.device === "Desktop",
+				}
+			)}
+			onClick={handleClick}
+		>
+			{state.editor.previewMode && state.editor.liveMode && (
+				<Button
+					variant={"ghost"}
+					size={"icon"}
+					className="w-6 h-6 bg-slate-600 p-[2px] fixed top-0 left-0 z-[100]"
+					onClick={handleUnpreview}
+				>
+					<EyeOff />
+				</Button>
+			)}
+
+		</div>
+	);
+}
--- app/[pageId]/page.tsx
+++ app/[pageId]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import PageEditorNavigation from "./_components/page-editor-navigation";
 import { createClient } from "@/utils/supabase/server";
 import PageEditorSidebar from "./_components/page-editor-sidebar";
+import PageEditor from "./_components/page-editor";
 
 type Props = {
 	params: Promise<{ pageId: string }>;
@@ -27,6 +28,11 @@ export default async function FunnelPage({ params }: Props) {
 		<div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
 			<EditorProvider pageId={pageId} pageDetails={pageDetails}>
 				<PageEditorNavigation pageDetails={pageDetails} pageId={pageId} />
+
+				<div className="h-full flex justify-center">
+					<PageEditor pageId={pageId} />
+				</div>
+
 				<PageEditorSidebar pageId={pageId} />
 			</EditorProvider>
 		</div>
